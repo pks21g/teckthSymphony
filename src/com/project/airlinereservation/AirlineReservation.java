18053ac035506e833a72b984c73f32a9bb0f57b6
usertest0447 <paulsmith414@yahoo.com>
2022-03-18 01:24:58 -0500

    implemented getName method for first name and last name

--- src/com/project/airlinereservation/AirlineReservation.java
+++ src/com/project/airlinereservation/AirlineReservation.java
@@ -1,10 +1,14 @@
 package com.project.airlinereservation;
 
+import java.util.Scanner;
+
 /**
  * AirlineReservation class     Reservation Console Program
  * @author                      Teckth Symphony
  */
 public class AirlineReservation {
+
+    private static Scanner scan = new Scanner(System.in);
 
     /**
      *
@@ -20,18 +24,40 @@ public class AirlineReservation {
         String localTimeStamp = "";     // stores system-based time date and zone
         int age = 0;                    // stores age
 
+        firstName = getName("Enter first name: ");
+        System.out.println(firstName);
+
+        lastName = getName(lastName);
+        System.out.println(lastName);
     }
 
     /**
-     *
      * @param name is a type string
-     *
      * @return true only if name contains letters containing a-z or A-Z and
      * the inclusive length of 2-15
      */
     public static boolean validateName(String name){
         if (name == null) return false;
         return name.matches("^[a-zA-Z]{2,15}");
+    }
+
+    /**
+     * @param message is a type string, and it is used to hold a prompt message
+     * @return valid alpha string: first name or last name depending on the usage
+     */
+    public static String getName(String message){
+
+        String name = "";
+        while(true) {
+
+            System.out.print(message);
+            name = scan.next();
+
+            if(validateName(name))
+                break;
+            System.out.println("Invalid " + message.substring(message.indexOf(" ") + 1) + "please try again");
+        }
+        return name;
     }
 
 }
